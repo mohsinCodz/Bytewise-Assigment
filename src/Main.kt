8f69468e1421204c749e295d1c7ac3da64e8b080
MOHSIN ALI <mohsinkerio3572@gmail.com>
2024-07-06 13:12:24 +0500

    Version_2

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,17 @@
-
 fun main() {
     val oldUrl = "play.kotlin.org/?id={id}/program={program}/device={device}"
     println("Here's the old url: $oldUrl")
     val id = 9
     val program = "android_dev"
     val device = "ios"
-    val currentUrl = buildUrl(oldUrl, id, program, device)
+    val currentUrl = oldUrl.buildUrl(id.toString(), program, device)
     println("\nHere's the Current url: $currentUrl")
 }
 
-fun buildUrl(oldUrl: String, id: Int, program: String, device: String): String {
-    val replacements = mapOf("id" to id.toString(), "program" to program, "device" to device)
-    return oldUrl.replace(Regex("""\{(\w+)\}""")) { matchResult ->
-        val key = matchResult.groupValues[1]
-        replacements[key] ?: ""
+fun String.buildUrl(vararg placeholders: String): String {
+    var formattedUrl = this
+    placeholders.forEachIndexed { index, value ->
+        formattedUrl = formattedUrl.replace("{${placeholders[index]}}", value)
     }
+    return formattedUrl
 }
