b5414bec2ff8626c64e961ebbb6116383edd9443
Mohsin Ali <155422244+mohsinCodz@users.noreply.github.com>
2024-07-06 13:23:48 +0500

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,17 @@
 fun main() {
     val oldUrl = "play.kotlin.org/?id={id}/program={program}/device={device}"
     println("Here's the old url: $oldUrl")
-    val id = 9
+    val id = 9.toString()
     val program = "android_dev"
     val device = "ios"
-    val currentUrl = oldUrl.buildUrl(id.toString(), program, device)
+    val currentUrl = oldUrl.buildUrl(id, program, device)
     println("\nHere's the Current url: $currentUrl")
 }
 
 fun String.buildUrl(vararg placeholders: String): String {
     var formattedUrl = this
     placeholders.forEachIndexed { index, value ->
-        formattedUrl = formattedUrl.replace("{${placeholders[index]}}", value)
+        formattedUrl = formattedUrl.replace(placeholders[index], value)
     }
     return formattedUrl
 }
